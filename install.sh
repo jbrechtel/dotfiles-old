11f6c6181e435580f864e0186c0c4c8fd4c3b393
James Brechtel <jbrechtel@gmail.com>
2015-01-13 16:23:27 -0500

    misc

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ ln -s ~/work/dotfiles/aliases ~/.aliases
 ln -s ~/work/dotfiles/zshrc ~/.zshrc
 ln -s ~/work/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/work/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/work/dotfiles/vimperatorrc ~/.vimperatorrc
+ln -s ~/work/dotfiles/emacs ~/.emacs.d
